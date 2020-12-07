ae5a0947dbca6e551491482ae46536ad43c0e775
Bruno Cadorette <brunocadorette@outlook.com>
2020-12-07 18:54:08 -0500

    modify day 7

--- src/Day7.hs
+++ src/Day7.hs
@@ -42,6 +42,8 @@ parseLine = do
   char '.'
   return (bagColor, xs)
 
+parseFile = fromJust . parseMaybe (parseLine `sepEndBy` newline)
+
 root = BagColor "shiny gold"
 
 createMapP1 :: [(BagColor, [(Int, BagColor)])] -> Map.Map BagColor [BagColor]
@@ -53,26 +55,15 @@ createMapP2 :: [(BagColor, [(Int, BagColor)])] -> Map.Map BagColor [(Int, BagCol
 createMapP2 =
   Map.fromListWith (++)
   
-
-createTreeP1 :: Ord a => a -> Map.Map a [a] -> Tree a
-createTreeP1 root m = 
-  case Map.lookup root m of
-    Just xs -> Node root (fmap (\x -> createTreeP1 x m) xs)
-    Nothing -> Node root [] 
-
-createTreeP2 :: Ord a => (Int, a) -> Map.Map a [(Int, a)] -> Tree (Int, a)
-createTreeP2 root m = 
-  case Map.lookup (snd root) m of
-    Just xs -> Node root (fmap (\x -> createTreeP2 x m) xs)
+createTreeFromMap :: Ord key => (a -> key) -> a -> Map.Map key [a] -> Tree a
+createTreeFromMap toKey root m = 
+  case Map.lookup (toKey root) m of
+    Just xs -> Node root (fmap (\x -> createTreeFromMap toKey x m) xs)
     Nothing -> Node root [] 
 
 solveP2 :: Int -> Tree Int -> Int
 solveP2 mult (Node n xs) = n' + sum (fmap (solveP2 n') xs)
   where n' = n * mult
     
-parseFile = fromJust . parseMaybe (parseLine `sepEndBy` newline)
-
-
-day7p1 = pred . length . nub . flatten . createTreeP1 root . createMapP1 . parseFile
-day7p2 = pred . solveP2 1 . fmap fst . createTreeP2 (1, root) . createMapP2 . parseFile
---testt = parseTest (string "bags" <|> string "bag") "bag"
+day7p1 = pred . length . nub . flatten . createTreeFromMap id root . createMapP1 . parseFile
+day7p2 = pred . solveP2 1 . fmap fst . createTreeFromMap snd (1, root) . createMapP2 . parseFile
